605ecc7b4a94b1550c81977697b0b139fc45ff34
thekareranjitw <thekareranjitw@gmail.com>
2023-02-10 23:53:30 +0545

    main files for this project

---
+++ openai_imagegenerator/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.css';
+import './App.css';
+import Index  from './component/Index';
+
+const App = () => {
+  return (
+    <div>
+    <Index/>
+    </div>
+  )
+}
+
+export default App
---
+++ openai_imagegenerator/src/component/Image.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Image = () => {
+  return (
+    <div>
+    .<div class="container">
+    
+        bs5-grid-row
+    </div>
+    </div>
+  )
+}
+
+export default Image
